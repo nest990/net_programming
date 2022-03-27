924e59c837be3a8f3596a5e0e048bcbdae830ecd
nest990 <101156999+nest990@users.noreply.github.com>
2022-03-27 12:31:41 +0900

    Add files via upload

---
+++ HW2_Limjeonghun/first_server2.py
@@ -0,0 +1,18 @@
+from http import client
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('',9000))
+s.listen(2)
+
+while True:
+    client, addr = s.accept()
+    print('Connection from', addr)
+    client.send(b'Hello '+ addr[0].encode())
+    msg = client.recv(1024)
+    print(msg.decode())
+    c = 20171708
+    d = c.to_bytes(4,'big')
+    client.send(d)
+
+    client.close()
